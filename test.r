199e5e93c3efe8dd7c74c24f135a41f80fc05515
Enterprise09 <goldenstaytes@gmail.com>
2021-12-12 14:28:16 +0900

    [+] test script & .pdf

---
+++ test.r
@@ -0,0 +1,12 @@
+getwd()
+
+install.packages("pdftools")
+install.packages("wordcloud")
+install.packages("tm")
+install.packages("RColorBrewer")
+
+library(pdftools)
+library(wordcloud)
+library(tm)
+library(RColorBrewer)
+
