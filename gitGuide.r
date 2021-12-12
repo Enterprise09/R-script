6a64b1d6913063e644a5541a3ae5d614a938c1b4
Enterprise09 <goldenstaytes@gmail.com>
2021-12-12 18:21:23 +0900

    complete coding

---
+++ gitGuide.r
@@ -0,0 +1,33 @@
+getwd()
+
+install.packages("pdftools")
+install.packages("wordcloud")
+install.packages("tm")
+install.packages("RColorBrewer")
+install.packages("stringr")
+
+library(pdftools)
+library(wordcloud)
+library(tm)
+library(RColorBrewer)
+library(stringr)
+
+sample <- pdf_text("git_guide.pdf")
+str <- str_c(sample, collapse=" ")
+no_space <- str_replace_all(str, "[[:space:]]{1,}", " ");
+
+# 한글을 제외한 결과를 보고 싶다면 아래의 주석을 제거하고 
+# 27줄의 'no_space'를 str_all로 변경하세요
+
+# str_extract_all(no_space, "[^[:ascii:]]{1,}")
+# str_all <- str_replace_all(no_space, "[^[:ascii:]]+", " ")
+
+colorCode <- brewer.pal(8, "Paired")
+
+wordcloud(
+    words = no_space,
+    min.freq = 5,
+    max.words = 400,
+    random.order = F,
+    colors = colorCode
+)
